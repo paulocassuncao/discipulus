c507a9a9c4dd7a03ecb5120178c3794532986dd2
Paulo Cavalcante <paulocassuncao@gmail.com>
2016-08-02 10:51:52 -0300

    Repo init

---
+++ Discipulus/Discipulus/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Discipulus.MvcApplication" Language="C#" %>
---
+++ Discipulus/Discipulus/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Discipulus.Startup))]
+namespace Discipulus
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
